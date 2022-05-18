b2bb51f647e0b4d38343bc5dd668bd4d8a48e299
Paweł Niedźwiecki <hello@uxu.pl>
2022-05-18 13:31:50 +0200

    update

--- next.config.js
+++ next.config.js
@@ -1,25 +1,4 @@
 module.exports = {
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
   reactStrictMode: true,
   images: {
     domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com"],
